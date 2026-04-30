0a1869d1150275bbbd0ca76dba8a689e10ea9fb4
Kody <kodymac920024@hotmail.com>
2026-04-29 19:15:28 -0500

    Initial Commit

---
+++ PageTurner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PageTurner
+//
+//  Created by Kody McNamara on 4/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PageTurner/PageTurnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageTurnerApp.swift
+//  PageTurner
+//
+//  Created by Kody McNamara on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct PageTurnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
